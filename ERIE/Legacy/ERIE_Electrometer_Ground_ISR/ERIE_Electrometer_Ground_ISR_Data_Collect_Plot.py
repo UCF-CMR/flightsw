ad5d0b2493c6a87b8a0acc42208fc797c55977cc
James Phillips <jrphill5@users.noreply.github.com>
2022-07-19 15:52:38 -0400

    Added script to simultaneously plot electrometer data while recording

---
+++ ERIE/Legacy/ERIE_Electrometer_Ground_ISR/ERIE_Electrometer_Ground_ISR_Data_Collect_Plot.py
@@ -0,0 +1,86 @@
+import serial, os
+from time import sleep
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+
+fn = datetime.now().strftime("%Y%m%d-%H%M%S-ISR") + ".csv"
+
+com = 6 # NOTE: Update this to match COM port for Arduino
+if os.path.exists("/dev/ttyS%d" % com):
+    ser = serial.Serial("/dev/ttyS%d" % com, baudrate=115200)
+else:
+    ser = serial.Serial("COM%d" % com, baudrate=115200)
+
+ser.setDTR(False)
+sleep(1)
+ser.flushInput()
+ser.setDTR(True)
+
+def parseLine(line):
+    try:
+        if len(line) == 3 and line[2].startswith("RESET"):
+            return int(line[0]), int(line[1]), line[2]
+        else:
+            line = [eval(x) for x in line]
+            return line[0], line[1], line[2:]
+    except:
+        return None, None, None
+
+plt.ion()
+
+fig = plt.figure(figsize=(16,8))
+ax = fig.add_subplot(111)
+
+plots = [ax.scatter([], [], marker='.', label=("Channel %d" % (c+1))) for c in range(8)]
+ax.set_xlim(0, 1)
+ax.set_ylim(0, 1024)
+ax.set_title("Real-Time Electrometer Measurements")
+ax.set_xlabel("Time (s)")
+ax.set_ylabel("ADC Reading")
+ax.legend()
+fig.set_tight_layout(True)
+fig.canvas.flush_events()
+plt.show(block=False)
+
+with open(fn, "w+") as fh:
+    while True:
+        try:
+            line = ser.readline()
+            line = [x.strip().decode() for x in line.split(b",")]
+            c, t, v = parseLine(line)
+            if c is None or t is None or v is None:
+                print(line)
+            else:
+                print(c, t, v)
+                if type(t) == int and t > 300000:
+                    break
+            if c is not None and t is not None and v is not None:
+                if len(v) == 5 or (type(v) == str and v.startswith("RESET")):
+                    fh.write(", ".join(line) + "\n")
+                    fh.flush()
+                else:
+                    print("[WARN] Skipping invalid data!")
+                if c == c:
+                    try:
+                        data = np.array([[t/1000., float(v[2])]])
+                    except ValueError:
+                        print("[WARN] Not plotting invalid data!")
+                    array = plots[c].get_offsets()
+                    array = np.append(array, data, axis=0)
+                    plots[c].set_offsets(array)
+
+                    ax.set_xlim(0, array[:,0].max() + 0.5)
+                    #ax.set_ylim(array[:,1].min() - 0.5, array[:,1].max() + 0.5)
+
+                    fig.canvas.flush_events()
+                    plt.show(block=False)
+            else:
+                print("[WARN] Skipping invalid data!")
+        except KeyboardInterrupt:
+            break
+
+plt.ioff()
+plt.show(block=True)
+
+ser.close()
